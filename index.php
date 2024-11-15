bdce76ae969744973ab40ae9bde2e20ffe769d40
JARP <andersonandy046@gmail.com>
2024-11-15 16:41:58 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php date_default_timezone_set("Etc/GMT+8");?>
+<?php date_default_timezone_set("Etc/GMT+8"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,58 +8,79 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Loan Management System</title>
-
-    <link href="css/all.css" rel="stylesheet" type="text/css">
-  
-   
-    <link href="css/sb-admin-2.css" rel="stylesheet">
+    <title>Microfinance Management System</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 
-<body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="">Loan Management System</a>
-	</nav>
-    <div class="container">
-		<div class="row justify-content-center">
-			<div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block"><img src="image/back1.jpg" height="100%" width="100%"/></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">USER LOGIN</h1>
-                                    </div>
-                                    <form method="POST" class="user" action="login.php">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" name="username" placeholder="Enter Username here..." required="required">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" name="password" placeholder="Enter Password here..." required="required">
-                                        </div>
-										<?php 
-											session_start();
-											if(ISSET($_SESSION['message'])){
-												echo "<center><label class='text-danger'>".$_SESSION['message']."</label></center>";
-											}
-										?>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block" name="login">Login</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+<body class="bg-gray-100">
+
+    <!-- Navbar -->
+    <nav class="bg-gray-900 text-white p-4">
+        <div class="container mx-auto">
+            <a class="text-2xl font-semibold" href="">Microfinance Management System</a>
+        </div>
+    </nav>
+
+ <!-- Main Content -->
+<div class="flex justify-center items-start pt-30"> <!-- pt-20 to add padding at the top -->
+    <div class="w-full max-w-lg bg-white rounded-lg shadow-lg p-6 mt-10">
+        <div class="grid grid-cols-1 md:grid-cols-1 gap-4">
+            <!-- Left Side Image -->
+            <div class="hidden md:block relative">
+                <!-- Background Image -->
+                <img src="image/background.jpg" alt="Background Image" class="rounded-lg w-full h-full object-cover">
+
+                <!-- User Login Text on Top of Image -->
+                <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl font-semibold text-black bg-white bg-opacity-90 px-6 py-3 rounded-md mb-4 flex items-center">
+                    <!-- Material Icon -->
+                    <span class="material-icons mr-2">account_circle</span>
+                    USER LOGIN
+                </h1>
+
+
+
+            </div>
+
+
+            <!-- Right Side Login Form -->
+            <div class="flex flex-col justify-center items-center">
+               
+                <form method="POST" class="w-full" action="login.php">
+                    <div class="mb-4">
+                        <input type="text" class="w-full p-3 border border-gray-300 rounded-md" name="username" placeholder="Enter Username here..." required>
                     </div>
-                </div>
+                    <div class="mb-4">
+                        <input type="password" class="w-full p-3 border border-gray-300 rounded-md" name="password" placeholder="Enter Password here..." required>
+                    </div>
+
+                    <?php 
+                    session_start();
+                    if (isset($_SESSION['message'])) {
+                        echo "<div class='text-red-600 text-center mb-4'>" . $_SESSION['message'] . "</div>";
+                    }
+                    ?>
+
+                    <button type="submit" class="w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 transition duration-300">Login</button>
+                </form>
             </div>
         </div>
     </div>
-	<nav class="navbar fixed-bottom navbar-dark bg-dark">
-		<label style="color:#ffffff;">&copy; Copyright Loan Management System</label>
-		<label style="color:#ffffff;">All Rights Reserved <?php echo date("Y")?> </label>
-	</nav>
+</div>
+
+
+
+    <!-- Footer -->
+    <nav class="bg-gray-900 text-white p-4 fixed bottom-0 w-full">
+        <div class="container mx-auto text-center">
+            <span>Microfinance Management System <?php echo date("Y"); ?></span>
+        </div>
+    </nav>
+
+
+
+    
 </body>
 
 </html>
