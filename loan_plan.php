43a53e4c0457ecb384c584c05d2c5571fd81e2a6
JARP <andersonandy046@gmail.com>
2024-11-15 18:33:31 +0800

    update

--- loan_plan.php
+++ loan_plan.php
@@ -32,11 +32,11 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-text mx-3">ADMIN PANEL</div>
+                <div class="sidebar-brand-text mx-3">ADMIN</div>
             </a>
 
 
