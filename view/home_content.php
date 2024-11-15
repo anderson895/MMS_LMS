b26227b20eb4eae1bfe39b1077425020256b71e4
JARP <andersonandy046@gmail.com>
2024-11-15 18:27:42 +0800

    update

--- view/home_content.php
+++ view/home_content.php
@@ -49,7 +49,7 @@
 
      
         <div class="col-xl-4 col-md-4 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
@@ -78,7 +78,7 @@
 
       
         <div class="col-xl-4 col-md-4 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
+            <div class="card shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
@@ -107,7 +107,7 @@
 
         
         <div class="col-xl-4 col-md-4 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
+            <div class="card shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
